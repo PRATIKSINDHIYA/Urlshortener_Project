d79544e591eda70c208e52a355e99c4f633ee849
PRATIKSINDHIYA <pratiksindhiya3@gmail.com>
2025-09-09 08:20:42 +0530

    ADD ABSOLUTE PATH

--- api/index.js
+++ api/index.js
@@ -1,8 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
 import { createshorturl, redirectshorturl } from "../Controller/url.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("views", path.join(__dirname, "../views")); // ab correct folder milega
+
 
 // MongoDB connection
 mongoose.connect("mongodb+srv://mydatabase:myrealdatabase@cluster0.nzmws71.mongodb.net/", {
